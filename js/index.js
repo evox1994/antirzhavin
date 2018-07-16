d641295e95073e7bf4394053c4b47d8a6c558478
evox1994 <dmitrybatukhtin@gmail.com>
2018-07-17 00:26:36 +0700

    dgbdgfb

---
+++ js/index.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+
+	$('.b-1-slider').slick();
+
+	$('.clients-slider').slick({
+		autoplay: true,
+		dots: true,
+		arrows: false,
+		slidesToShow: 3,
+	});
+
+	$('.b-6-slider').slick({
+		autoplay: true,
+		dots: true,
+		arrows: false,
+		slidesToShow: 3,
+	});
+
+});
---
+++ js/main.js
@@ -0,0 +1,4 @@
+$(document).ready(function(){
+    var y = new Date();
+    $('.footer-info-left span b').text( y.getFullYear() );
+});
