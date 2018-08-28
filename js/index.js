b85b3d1712034be9ba8024a8c88441d5f32ce083
evox1994 <dmitrybatukhtin@gmail.com>
2018-08-28 14:53:17 +0700

    dfbdfbdfb

--- js/index.js
+++ js/index.js
@@ -35,4 +35,32 @@ $(document).ready(function(){
 		}]
 	});
 
+	function product() {
+		if ( $(window).width() > 768 ) {
+			$('.b').each(function(){
+				var th, dh, ih;
+				th = dh = ih = 0;
+				$(this).find('.product').each(function(){
+					if ( $(this).find('.product-name').height() > th ) {
+						th = $(this).find('.product-name').height();
+					}
+					if ( $(this).find('.product-description').height() > dh ) {
+						dh = $(this).find('.product-description').height();
+					}
+					if ( $(this).find('ul').height() > ih ) {
+						ih = $(this).find('ul').height();
+					}
+				});
+				$(this).find('.product ul').height(ih);
+				$(this).find('.product-description').height(dh);
+				$(this).find('.product-name').height(th);
+			});
+		}
+	}
+	product();
+
+	$(window).resize(function(){
+		product();
+	});
+
 });
